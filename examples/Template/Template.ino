97a222a130189082ddb9a4f84d09d5d422f59326
Robert Sorić <robert.soric@gmail.com>
2022-11-16 12:00:49 +0100

    Initial commit, followed the template to create necessary files

--- examples/Template/Template.ino
+++ examples/Template/Template.ino
@@ -9,12 +9,31 @@
  * @authors     @ soldered.com
  ***************************************************/
 
-#include "Generic-easyC-SOLDERED.h"
+#include "Position-sensor-AS5600-breakout-SOLDERED.h"
+
+PositionSensor as5600;
+AS5600 mysensor
 
 void setup()
 {
+    Serial.begin(115200);
+    delay(100);
+
+    if(!mysensor.begin(0))
+    {
+        Serial.println("not connected");
+        while(1)
+        {
+            delay(100);
+        }
+    }
+    
+    mysensor.setDirection(AS5600_CLOCK_WISE);
 }
 
 void loop()
 {
+    Serial.println(mysensor.readAngle());
+    Serial.println(mysensor.rawAngle()*AS5600_RAW_TO_DEGREES);
+    delay(100);
 }
---
+++ src/Position-sensor-AS5600-breakout-SOLDERED.cpp
@@ -0,0 +1,19 @@
+/**
+ **************************************************
+ *
+ * @file        Generic-easyC-SOLDERED.cpp
+ * @brief       Example functions to overload in base class.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     @ soldered.com
+ ***************************************************/
+
+
+#include "Position-sensor-AS5600-breakout-SOLDERED.h"
+
+bool AS5600::begin()
+{
+    
+    return true;
+}
